a1036f4c64407f15ff31a56756b17f060e67acc3
Adnan <ak567845@gmail.com>
2024-11-11 22:42:50 +0530

    "Added styles for gradient effect and updated LandingPage component with new layout and content"

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/Logo.png";
+import { Button } from "@/components/ui/button";
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    
+    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-10 ">
+      <section className="text-center ">
+        <h1 className="flex flex-col text-4xl sm:text-6xl py-4 font-bold   lg:text-8xl items-center justify-center gradient-title">
+          <span className="flex flex-row">
+            Welcome to
+            <img src={logo} alt="logo" className="h-14 -mt-6 sm:h-24 lg:h-32" />
+            and
+          </span>
+          <span className="sm:-mt-0 md:-mt-4 -mt-2 ">get a job </span>
+        </h1>
+        <p className="text-gray-300 text-xs sm:text-xl  sm:mt-4">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
+      </section>
+      <div className="">
+        <Link to="/jobs">
+          <Button>Find Jobs</Button>
+        </Link>
+        <Link to="/jobs">
+          <Button>Post a Job</Button>
+        </Link>
+      </div>
+      <section className=""></section>
+    </main>
+  );
 };
 
 export default LandingPage;
